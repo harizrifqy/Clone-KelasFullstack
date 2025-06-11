0feac9d1dc8419985523d69086c8fc58707834c3
anaktjkt <anaktjkt@gmail.com>
2025-06-11 15:52:17 +0700

    feat: add payment page, coupon logic, and unify title-section styling

--- js/main.js
+++ js/main.js
@@ -4,4 +4,5 @@ const navbarLink = document.querySelector('.navbar-link');
 navToggle.addEventListener('click', () => {
   navToggle.classList.toggle('active');
   navbarLink.classList.toggle('active');
-});
+})
+
