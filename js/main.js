a917b27be5d226eba68825574cb34b135b65665e
anaktjkt <anaktjkt@gmail.com>
2025-06-26 08:06:07 +0700

    feat: Add login page + moved hamburger menu script inline

--- js/main.js
+++
@@ -1,8 +0,0 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navbarLink = document.querySelector('.navbar-link');
-
-navToggle.addEventListener('click', () => {
-  navToggle.classList.toggle('active');
-  navbarLink.classList.toggle('active');
-})
-
